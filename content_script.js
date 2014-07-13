4e7c05adec081a6bb08406ad9e06c40f6a8d7848
LizCira <liz.cira.goldstein@gmail.com>
2014-07-13 16:12:00 -0400

    committing before restructuring logic to store keyword values in html. go germany

--- content_script.js
+++ content_script.js
@@ -1,63 +1,12 @@
 breakingBad = ["Breaking Bad", "Walter", "Walter White", "Skyler", "Jesse Pinkman", "Walt", "Jesse", "Saul"]
 madMen = ["Mad Men", "Don Draper"]
+gameOfThrones = ["Game of Thrones", "Red Wedding"]
 
-// var selections = []
+var selections = [];
 
-// var addModule = function() {
-//   //saving the checkbox value
-//   var x = ( $( "input:checked" ).val());
-
-//   // checking if it's already been selected
-//   if($.inArray(x, selections)){
-//     var index = selections.indexOf(x);
-//     selections.splice(index, 1)
-// } else {
-//     console.log(x)
-//     //adding if not found
-//     selections.push(x)
-//     console.log(selections);
-// }}
-
-var selections = []
-
-window.addEventListener('load', function(evt) {
-
-  $('input:checkbox').change(function(){
-    console.log(this)
-
-    var clicked = this
-
-    if(clicked.checked === true){
-        var selection = clicked["value"]
-        console.log(selection);
-        selections.push(selection);
-        console.log(selections);
-        // return selections
-        }else{
-        var index = selections.indexOf(selection);
-        selections.splice(index, 1);
-        console.log(selections);
-        // return selections
-        }
-
-    // var x = ( $("input:checked").val());
-    // console.log(x)
-    // selections.push(x)
-    // console.log(selections);
-});
-    // if(this.checked)
-    //     console.log( $( "input:checked" ).val() + " checked!" );
-    // else
-    //     console.log('not checked');
-    // });
-
-});
-
-
-// $.each(selections, function(selected))
-
-$.each(breakingBad, function(index, keyword) {
-  // debugger
+function filter(module){
+    $.each(module, function(index, keyword) {
+        console.log("Keyword we're filtering: " + keyword)
     $( "h1:contains('" + keyword + "')" ).css( "background", "black" );
     $( "h1:contains('" + keyword + "')" ).css( "color", "black" );
     $( "h2:contains('" + keyword + "')" ).css( "background", "black" );
@@ -68,5 +17,71 @@ $.each(breakingBad, function(index, keyword) {
     $( "p:contains('" + keyword + "')" ).css( "color", "black" );
     $( "p:contains('" + keyword + "') > a " ).css("color", "black");
 })
+}
+
+
+
+window.addEventListener('load', function(evt) {
+
+  $('input:checkbox').change(function(){
+    console.log(this + " is what you just clicked")
+
+    //saving clicked element here
+    var clicked = this
+
+    if(clicked.checked === true){
+        var name = clicked["name"]
+        var selection = clicked["value"]
+        console.log("Adding to selection array: " + selection);
+        // debugger
+        selections.push(selection);
+        // console.log(selections);
+        }else{
+        var index = selections.indexOf(selection);
+        selections.splice(index, 1);
+        // console.log(selections);
+        }
+
+    //array is saved correctly here!
+    console.log("These are in the selections array: " + selections);
+
+
+    $.each(selections, function(index, selection){
+        console.log(selection + " that we're about to filter");
+        // if(selection === "madMen")
+        // madMen = ["Mad Men", "Don Draper"]
+        // module = madMen
+        // debugger
+        filter(module);
+    })
+    // return selections;
+    });
+  //but not here :(
+   // console.log("One scope up, these are in selections array: " + selections);
+
+   //saving nice checked/unchecked syntax
+    // if(this.checked)
+    //     console.log( $( "input:checked" ).val() + " checked!" );
+    // else
+    //     console.log('not checked');
+    // });
+});
+
+
+// $.each(selections, function(selected))
+
+// $.each(breakingBad, function(index, keyword) {
+//   // debugger
+//     $( "h1:contains('" + keyword + "')" ).css( "background", "black" );
+//     $( "h1:contains('" + keyword + "')" ).css( "color", "black" );
+//     $( "h2:contains('" + keyword + "')" ).css( "background", "black" );
+//     $( "h2:contains('" + keyword + "')" ).css( "color", "black" );
+//     $( "h3:contains('" + keyword + "')" ).css( "background", "black" );
+//     $( "h3:contains('" + keyword + "')" ).css( "color", "black" );
+//     $( "p:contains('" + keyword + "')" ).css( "background", "black" );
+//     $( "p:contains('" + keyword + "')" ).css( "color", "black" );
+//     $( "p:contains('" + keyword + "') > a " ).css("color", "black");
+// })
+
 
 
