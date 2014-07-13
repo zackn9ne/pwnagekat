23603a11c449908f11865e12ec3c203e156c455d
LizCira <liz.cira.goldstein@gmail.com>
2014-07-13 01:08:19 -0400

    checkbox/ jquery logic in progress.. finding selected checks and .change event listener works

--- content_script.js
+++ content_script.js
@@ -1,12 +1,39 @@
-breakingBad = ["Breaking Bad", "Walter", "Walter White", "Skyler", "Jesse Pinkman", "Walt", "Jesse", "Saul"]
+breakingBad = ["Breaking Bad", "Walter", "Walter White", "Skyler", "Jesse Pinkman", "Walt", "Jesse", "Saul", "#breakingbad"]
 madMen = ["Mad Men", "Don Draper"]
 
+var selections = []
 
-// var selections = []
+var addModule = function() {
+  //saving the checkbox value
+  var x = ( $( "input:checked" ).val());
+
+  // checking if it's already been selected
+  if ($.inArray(x, selections)){
+    var index = selections.indexOf(x);
+    selections.splice(index, 1)
+} else {
+    console.log(x)
+    //adding if not found
+    selections.push(x)
+    console.log(selections);
+}}
+
+$( document ).ready(function() {
+
+$('input:checkbox').change(function(){
+   if(this.checked)
+      console.log('checked');
+   else
+      console.log('not checked');
+});
+
+});
+
 
 // $.each(selections, function(selected))
 
 $.each(breakingBad, function(keyword) {
+  // debugger
     $( "h1:contains('" + keyword + "')" ).css( "background", "black" );
     $( "h1:contains('" + keyword + "')" ).css( "color", "black" );
     $( "h2:contains('" + keyword + "')" ).css( "background", "black" );
