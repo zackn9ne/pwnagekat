91e172caac3562f7b5d04e38c1827029196701ef
LizCira <liz.cira.goldstein@gmail.com>
2014-07-16 17:41:03 -0400

    added persistance for new checkboxes

--- checkboxStorage.js
+++ checkboxStorage.js
@@ -65,6 +65,125 @@ $(document).ready(function() {
       }
     });
 
+   $(function () {
+      var data = localStorage.getItem("goodWife");
+
+      if (data !== null) {
+          $("input[id='goodWife']").attr("checked", "checked");
+      }
+  });
+
+  $("input[id='goodWife']").click(function () {
+
+      if ($(this).is(":checked")) {
+          localStorage.setItem("goodWife", $(this).val());
+      } else {
+          localStorage.removeItem("goodWife");
+      }
+    });
+
+  $(function () {
+      var data = localStorage.getItem("scandal");
+
+      if (data !== null) {
+          $("input[id='scandal']").attr("checked", "checked");
+      }
+  });
+
+  $("input[id='scandal']").click(function () {
+
+      if ($(this).is(":checked")) {
+          localStorage.setItem("scandal", $(this).val());
+      } else {
+          localStorage.removeItem("scandal");
+      }
+    });
+
+    $(function () {
+      var data = localStorage.getItem("trueBlood");
+
+      if (data !== null) {
+          $("input[id='trueBlood']").attr("checked", "checked");
+      }
+  });
+
+  $("input[id='trueBlood']").click(function () {
+
+      if ($(this).is(":checked")) {
+          localStorage.setItem("trueBlood", $(this).val());
+      } else {
+          localStorage.removeItem("trueBlood");
+      }
+    });
+
+  $(function () {
+      var data = localStorage.getItem("drWho");
+
+      if (data !== null) {
+          $("input[id='drWho']").attr("checked", "checked");
+      }
+  });
+
+  $("input[id='drWho']").click(function () {
+
+      if ($(this).is(":checked")) {
+          localStorage.setItem("drWho", $(this).val());
+      } else {
+          localStorage.removeItem("drWho");
+      }
+    });
+
+  $(function () {
+      var data = localStorage.getItem("yankees");
+
+      if (data !== null) {
+          $("input[id='yankees']").attr("checked", "checked");
+      }
+  });
+
+  $("input[id='yankees']").click(function () {
+
+      if ($(this).is(":checked")) {
+          localStorage.setItem("yankees", $(this).val());
+      } else {
+          localStorage.removeItem("yankees");
+      }
+    });
+
+  $(function () {
+      var data = localStorage.getItem("redSox");
+
+      if (data !== null) {
+          $("input[id='redSox']").attr("checked", "checked");
+      }
+  });
+
+  $("input[id='redSox']").click(function () {
+
+      if ($(this).is(":checked")) {
+          localStorage.setItem("redSox", $(this).val());
+      } else {
+          localStorage.removeItem("redSox");
+      }
+    });
+
+   $(function () {
+      var data = localStorage.getItem("dancingWithTheStars");
+
+      if (data !== null) {
+          $("input[id='dancingWithTheStars']").attr("checked", "checked");
+      }
+  });
+
+  $("input[id='dancingWithTheStars']").click(function () {
+
+      if ($(this).is(":checked")) {
+          localStorage.setItem("dancingWithTheStars", $(this).val());
+      } else {
+          localStorage.removeItem("dancingWithTheStars");
+      }
+    });dancingWithTheStars
+
 });
 
 
